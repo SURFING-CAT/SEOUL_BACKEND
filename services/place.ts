a64d3553ecc6beb1331b54062a44a28ec3b53c2e
unknown <yjs12180825@gmail.com>
2024-05-06 15:00:09 +0900

    :memo: services/place 뼈대 작성 :memo:

--- services/place.ts
+++ services/place.ts
@@ -5,7 +5,22 @@ export default class PlaceService {
   constructor() {}
 
   /***
+   * 자정이 지나면 다시 위치를 선택
+   */
+  public async SelectTodayPlace() {}
+
+  /***
    *  2 x 2 퍼즐 생성
    */
   public async CreatePlacePuzzle() {}
+
+  /***
+   * 현재 공개되지 않은 퍼즐 조각 중 하나를 리턴
+   */
+  public async GetRandomPuzzlePiece() {}
+
+  /***
+   *  현재 위치를 비교해 유저에게 보상 부여
+   */
+  public async GiveRewards() {}
 }
