9bad8a8b60514b70b817fc12313a3f1bbe796e5f
unknown <yjs12180825@gmail.com>
2024-05-06 14:44:08 +0900

    :sparkles: 의존성 주입 , place model 생성 :sparkles:

--- config/index.ts
+++ config/index.ts
@@ -12,4 +12,16 @@ export default {
   api: {
     prefix: "/api/v1",
   },
+  logs: {
+    level: process.env.LOG_LEVEL || "silly",
+  },
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+  },
 };
--- loaders/index.ts
+++ loaders/index.ts
@@ -1,6 +1,24 @@
 import { Application } from "express";
 import expressLoader from "./express";
+import firebaseLoader from "./firebase";
+import Logger from "./logger";
+import dependencyInjector from "./dependencyInjector";
 
 export default async ({ expressApp }: { expressApp: Application }) => {
+  const firebaseConnection = await firebaseLoader();
+  Logger.info("📣 Firebase connected! ");
+
+  const placeModel = {
+    name: "placeModel",
+    model: require("../models/place").default,
+  };
+
+  await dependencyInjector({
+    firebaseConnection,
+    models: [placeModel],
+  });
+  Logger.info("📣 Dependency Injector loaded! ");
+
   await expressLoader({ app: expressApp });
+  Logger.info("📣 Express loaded! ");
 };
