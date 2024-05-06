3dd8df2b6a269de557c070ac5c471fd01fe27b2c
unknown <yjs12180825@gmail.com>
2024-05-06 13:09:29 +0900

    :memo: services/auth.ts , service/treasure.ts :memo:

---
+++ services/treasure.ts
@@ -0,0 +1,11 @@
+import { Inject, Service } from "typedi";
+
+@Service()
+export default class TreasureService {
+  constructor() {}
+
+  /**
+   *  2 x 2 퍼즐 생성
+   */
+  public async CreatePuzzle() {}
+}
